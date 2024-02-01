4db5ac39392e2f2daae906819a8c76388f2a057e
Joshua <Doerak1aug@gmail.com>
2024-02-01 21:04:55 +0100

    V1.1
    Finished 004 and 005

--- Week1/004.Rainbow/src/Rainbow.java
+++ Week1/004.Rainbow/src/Rainbow.java
@@ -5,23 +5,61 @@ import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
+import java.awt.*;
+import java.awt.geom.Line2D;
+
 public class Rainbow extends Application {
+    private Canvas canvas;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        this.canvas = new Canvas(1920-800, 1080-500);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Rainbow");
         primaryStage.show();
     }
-    
-    
+
+
     public void draw(FXGraphics2D graphics) {
 
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()-50);
+        graphics.scale( -0.3, -0.3);
+
+//        graphics.setColor(Color.red);
+//        graphics.drawLine(0,0,1000,0);
+//        graphics.setColor(Color.green);
+//        graphics.drawLine(0,0,0,1000);
+//        graphics.setColor(Color.black);
+
+
+        double resolution = 0.05;
+        double scale = 500.0;
+
+        double radiusBinnen = 1.2;
+        double radiusBuiten = 2.2;
+
+        graphics.draw(new Line2D.Double(0.5*scale, 0*scale, 3.5*scale, 0*scale));
+        graphics.draw(new Line2D.Double(0*scale, 1*scale, 0*scale, 3.5*scale));
+        graphics.draw(new Line2D.Double(-0.5*scale, 0*scale, -3.5*scale, 0*scale));
+
+        for(double hoek = 0; hoek <= 180; hoek+=resolution)
+        {
+
+            graphics.setColor(Color.getHSBColor((float) -(hoek/180),1.0f,1.0f));
+
+            double rad = Math.toRadians(hoek);
+            float x1 = (float) (radiusBuiten * Math.cos(rad));
+            float y1 = (float) (radiusBuiten * Math.sin(rad));
+            float x2 = (float) (radiusBinnen * Math.cos(rad));
+            float y2 = (float) (radiusBinnen * Math.sin(rad));
+
+            graphics.draw(new Line2D.Double(x1*scale, y1*scale, x2*scale, y2*scale));
+        }
     }
-    
-    
-    
+
+
+
+
     public static void main(String[] args) {
         launch(Rainbow.class);
     }
--- Week1/005.Spirograph/src/Spirograph.java
+++ Week1/005.Spirograph/src/Spirograph.java
@@ -8,27 +8,41 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
+import java.awt.*;
+import java.awt.geom.Line2D;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
 public class Spirograph extends Application {
     private TextField v1;
     private TextField v2;
     private TextField v3;
     private TextField v4;
+    private Canvas canvas;
+    private  VBox mainBox;
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1080-100, 1080-100);
        
-        VBox mainBox = new VBox();
+        mainBox = new VBox();
         HBox topBar = new HBox();
         mainBox.getChildren().add(topBar);
-        mainBox.getChildren().add(new Group(canvas));
+        mainBox.getChildren().add(canvas);
         
-        topBar.getChildren().add(v1 = new TextField("300"));
-        topBar.getChildren().add(v2 = new TextField("1"));
-        topBar.getChildren().add(v3 = new TextField("300"));
-        topBar.getChildren().add(v4 = new TextField("10"));
+        topBar.getChildren().add(v1 = new TextField("200")); //main arm offset
+        topBar.getChildren().add(v2 = new TextField("1"));  //main arm angle
+        topBar.getChildren().add(v3 = new TextField("400")); //second arm offset
+        topBar.getChildren().add(v4 = new TextField("4")); //second arm angle (# of loops-1?)
+
+        FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
+        graphics.translate(canvas.getWidth()/2, canvas.getHeight()/2); //using translate in draw() keeps moving it further
+        graphics.scale( 0.8, 0.8);
                 
-        v1.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
+        v1.textProperty().addListener(e -> {
+            draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+        });
         v2.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
         v3.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
         v4.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
@@ -38,15 +52,64 @@ public class Spirograph extends Application {
         primaryStage.setTitle("Spirograph");
         primaryStage.show();
     }
-    
-    
+
+    //you can use Double.parseDouble(v1.getText()) to get a double value from the first textfield
+
     public void draw(FXGraphics2D graphics) {
-        //you can use Double.parseDouble(v1.getText()) to get a double value from the first textfield
-        //feel free to add more textfields or other controls if needed, but beware that swing components might clash in naming
+//        mainBox.getChildren().remove(1);
+//        this.canvas = new Canvas(1080-100, 1080-100);
+//        mainBox.getChildren().add(canvas);
+
+
+//        graphics.setColor(Color.red);
+//        graphics.drawLine(0,0,1000,0);
+//        graphics.setColor(Color.green);
+//        graphics.drawLine(0,0,0,1000);
+        graphics.setColor(Color.getHSBColor((float) Math.random(), 1.0f, 1.0f));
+
+        double resolution = 0.1;
+        double scale = 0.9;
+
+        double a = Double.parseDouble(v1.getText());
+        double b = Double.parseDouble(v2.getText());
+        double c = Double.parseDouble(v3.getText());
+        double d = Double.parseDouble(v4.getText());
+
+        float lastX = (float) (a * cos(Math.toRadians(b) * 0) + c * cos(Math.toRadians(d) * 0));
+        float lastY = (float) (a * sin(Math.toRadians(b) * 0) + c * sin(Math.toRadians(d) * 0));
+
+//        double rad1 = Math.toRadians(b);
+//        double rad2 = Math.toRadians(d);
+//        float x1 = (float) (a * Math.cos(rad1*0));
+//        float y1 = (float) (a * Math.sin(rad1*0));
+//        float x2 = (float) (c * Math.cos(rad2*0));
+//        float y2 = (float) (c * Math.sin(rad2*0));
+//
+//        float lastX = x1 + x2;
+//        float lastY = y1 + y2;
+
+        for(double phi = 0; phi < 10000; phi += resolution)
+        {
+//             rad1 = Math.toRadians(b);
+//             rad2 = Math.toRadians(d);
+//             x1 = (float) (a * Math.cos(rad1*phi));
+//             y1 = (float) (a * Math.sin(rad1*phi));
+//             x2 = (float) (c * Math.cos(rad2*phi));
+//             y2 = (float) (c * Math.sin(rad2*phi));
+//
+//            float x = x1 + x2;
+//            float y = y1 + y2;
+            float x = (float) (a * cos(Math.toRadians(b) * phi) + c * cos(Math.toRadians(d) * phi));
+            float y = (float) (a * sin(Math.toRadians(b) * phi) + c * sin(Math.toRadians(d) * phi));
+            graphics.draw(new Line2D.Double(x*scale, y*scale, lastX*scale, lastY*scale));
+            lastX = x;
+            lastY = y;
+        }
     }
-    
-    
-    
+
+
+
+
     public static void main(String[] args) {
         launch(Spirograph.class);
     }
