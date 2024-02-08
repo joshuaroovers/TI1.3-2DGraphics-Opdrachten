fda7535f7dc5528f99b587a200fb45523812d5b8
Joshua <Doerak1aug@gmail.com>
2024-02-08 22:43:53 +0100

    V2.3
    finished 003 and 004

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 
@@ -25,6 +26,7 @@ public class Colors extends Application {
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("Colors");
         primaryStage.show();
+
     }
 
 
@@ -33,6 +35,38 @@ public class Colors extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        double width = canvas.getWidth()/13; // 13 == amount of colors
+        //double height = canvas.getHeight();
+        double height = width;
+        int y = 0;
+        drawStandardColours(graphics, y, width, height);
+    }
+
+    public void drawStandardColours(FXGraphics2D graphics, int y, double width, double height){
+        ArrayList<Color> colors = new ArrayList<>();
+
+        colors.add(Color.MAGENTA);
+        colors.add(Color.PINK);
+        colors.add(Color.RED);
+        colors.add(Color.ORANGE);
+        colors.add(Color.YELLOW);
+        colors.add(Color.GREEN);
+        colors.add(Color.CYAN);
+        colors.add(Color.BLUE);
+        colors.add(Color.BLACK);
+        colors.add(Color.DARK_GRAY);
+        colors.add(Color.GRAY);
+        colors.add(Color.LIGHT_GRAY);
+        colors.add(Color.WHITE);
+
+
+        for (int i = 0; i < colors.size(); i++) {
+            Rectangle2D square = new Rectangle2D.Double(width*i, y, width, height);
+            graphics.setColor(colors.get(i));
+            graphics.fill(square);
+            graphics.draw(square);
+        }
     }
 
 
--- Week2/004.GradientPaint/src/GradientPaintExercise.java
+++ Week2/004.GradientPaint/src/GradientPaintExercise.java
@@ -3,6 +3,7 @@ import java.awt.geom.*;
 
 import javafx.application.Application;
 
+import static java.awt.SystemColor.window;
 import static javafx.application.Application.launch;
 
 import javafx.scene.Group;
@@ -19,20 +20,44 @@ public class GradientPaintExercise extends Application {
     public void start(Stage primaryStage) throws Exception
     {
         BorderPane mainPane = new BorderPane();
-        canvas = new ResizableCanvas(g -> draw(g), mainPane);
+        canvas = new ResizableCanvas(g -> draw(g,canvas.getWidth()/2,canvas.getHeight()/2), mainPane);
         mainPane.setCenter(canvas);
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("GradientPaint");
         primaryStage.show();
-        draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+
+        draw(new FXGraphics2D(canvas.getGraphicsContext2D()),canvas.getWidth()/2,canvas.getHeight()/2);
+        canvas.setOnMouseDragged(e ->{
+
+            draw(new FXGraphics2D(canvas.getGraphicsContext2D()), MouseInfo.getPointerInfo().getLocation().x-primaryStage.getX(), MouseInfo.getPointerInfo().getLocation().y-primaryStage.getY());
+        });
     }
 
 
-    public void draw(FXGraphics2D graphics)
+    public void draw(FXGraphics2D graphics, double focusX, double focusY)
     {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        drawGradiant(graphics, focusX, focusY);
+    }
+
+
+    public void drawGradiant(FXGraphics2D graphics, double focusX, double focusY){
+        double width = canvas.getWidth();
+        double height = canvas.getHeight();
+
+        Rectangle2D rectangle = new Rectangle2D.Double(0,0, width, height);
+
+        Point2D focus = new Point2D.Double(focusX,focusY);
+        float radius = 50;
+        float[] dist = {0.16f, 0.32f, 0.48f, 0.64f, 0.80f, 0.96f};
+        Color[] colors = {Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.ORANGE, Color.RED};
+        RadialGradientPaint paint = new RadialGradientPaint(focus, radius, dist, colors, MultipleGradientPaint.CycleMethod.REPEAT);
+        graphics.setPaint(paint);
+        graphics.fill(rectangle);
+        graphics.draw(rectangle);
     }
 
 
