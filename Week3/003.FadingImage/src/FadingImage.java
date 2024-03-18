3d05a38a54e30f042c3360c2c669e2102ec1a161
Joshua <Doerak1aug@gmail.com>
2024-03-18 14:13:35 +0100

    V3.11
    finsihed 003

--- Week3/003.FadingImage/src/FadingImage.java
+++ Week3/003.FadingImage/src/FadingImage.java
@@ -2,6 +2,8 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -15,18 +17,36 @@ import org.jfree.fx.ResizableCanvas;
 
 public class FadingImage extends Application {
     private ResizableCanvas canvas;
-    private FadingImageSet imageSet;
+    private ArrayList<FadingImageSet> imageSets;
     
     @Override
     public void start(Stage stage) throws Exception {
 
         BorderPane mainPane = new BorderPane();
+        mainPane.setPrefWidth(650);  //todo tried resizing the canvas but that gave an error because of the immediate draw() call.
+        mainPane.setPrefHeight(650);
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
+
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+        imageSets = new ArrayList<>();
 
-        imageSet = new FadingImageSet((int)canvas.getWidth()/2, (int)canvas.getHeight()/2, (int)canvas.getWidth()/2, (int)canvas.getHeight()/2);
-        imageSet.addImage("Olarya Weaveling 2.png");
+        FadingImageSet imageSet1 = new FadingImageSet(0, 0, (int)canvas.getWidth()/2, (int)canvas.getHeight()/2, 0.5);
+        imageSets.add(imageSet1);
+        imageSet1.addImage("Olarya Fallen.png");
+        imageSet1.addImage("Olarya Weaveling 2.png");
+        imageSet1.addImage("Olarya Fallen.png");
+        imageSet1.addImage("Olarya Ganaro.png");
+
+        FadingImageSet imageSet2 = new FadingImageSet((int)canvas.getWidth()/2, 0, (int)canvas.getWidth()/2, (int)canvas.getHeight()/2);
+        imageSets.add(imageSet2);
+        imageSet2.addImage("Eurydice Nightmare.png");
+        imageSet2.addImage("Eurydice Onasis.png");
+
+        FadingImageSet imageSet3 = new FadingImageSet(0, (int)canvas.getWidth()/2, (int)canvas.getWidth()/2, (int)canvas.getHeight()/2);
+        imageSets.add(imageSet3);
+        imageSet3.addImage("Cleopatra.png");
+        imageSet3.addImage("Cleo.png");
 
         new AnimationTimer() {
             long last = -1;
@@ -52,13 +72,18 @@ public class FadingImage extends Application {
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int)canvas.getWidth(), (int)canvas.getHeight());
 
-        if(imageSet != null)
-            imageSet.draw(graphics);
+        for (FadingImageSet imageSet : imageSets) {
+            if(imageSet != null)
+                imageSet.draw(graphics);
+        }
     }
     
 
     public void update(double deltaTime) {
-	
+        for (FadingImageSet imageSet : imageSets) {
+            imageSet.update(deltaTime);
+        }
+
     }
 
     public static void main(String[] args) {
--- Week3/003.FadingImage/src/FadingImageSet.java
+++ Week3/003.FadingImage/src/FadingImageSet.java
@@ -1,5 +1,3 @@
-import javafx.scene.image.Image;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -8,34 +6,39 @@ import java.util.ArrayList;
 
 public class FadingImageSet {
 
+
     private ArrayList<BufferedImage> images;
     private int currentImage;
+    private int secondImage;
+    private float opacity;
+    private double speed;
     private int posX;
     private int posY;
     private int width;
     private int height;
 
     //fades from 1 image to image 2, image 2 to image 3, etc.
-    public FadingImageSet(ArrayList<BufferedImage> images, int posX, int posY, int width, int height) {
+    public FadingImageSet(ArrayList<BufferedImage> images, int posX, int posY, int width, int height, double speed) {
         this.images = images;
         this.currentImage = 0;
+        this.secondImage = -1;
+        this.opacity = 0.0f;
+        this.speed = speed;
         this.posX = posX;
         this.posY = posY;
         this.width = width;
         this.height = height;
     }
 
+    public FadingImageSet(int posX, int posY, int width, int height, double speed) {
+        this(new ArrayList<>(), posX, posY, width, height, speed);
+    }
     public FadingImageSet(int posX, int posY, int width, int height) {
-        this.images = new ArrayList<>();
-        this.currentImage = 0;
-        this.posX = posX;
-        this.posY = posY;
-        this.width = width;
-        this.height = height;
+        this(posX, posY, width, height, 1.0);
     }
 
-    public BufferedImage getCurrentImage(){
-        return images.get(currentImage);
+    public BufferedImage getImage(int index){
+        return images.get(index);
     }
 
     public void addImage(String imageName){
@@ -45,30 +48,53 @@ public class FadingImageSet {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        this.secondImage = images.size()-1;
     }
 
-    public void update(){
-        currentImage++;
+    public void update(double deltaTime){
+//        System.out.println(opacity);
+        if(opacity >= 1.0f){
+            System.out.println("next image");
+            if(currentImage  == images.size()-1){
+                currentImage = 0;
+                secondImage = images.size()-1;
+            }else{
+                currentImage++;
+                secondImage = currentImage-1;
+            }
+
+            opacity = 0.0f;
+        }else{
+            opacity += deltaTime*speed;
+        }
     }
 
     public void draw(Graphics2D g2d){
-        BufferedImage image = this.getCurrentImage();
+        BufferedImage image = this.getImage(currentImage);
+        BufferedImage image2 = this.getImage(secondImage);
         AffineTransform transform = new AffineTransform();
 
 
         double widthScale = (double)this.width/image.getWidth();
         double heightScale = widthScale; //(double)this.height/image.getHeight();
 
-        System.out.println("original size: "+image.getWidth()+", "+image.getHeight());
-        System.out.println("target* size: "+this.width+", "+this.height);
-        System.out.println("scale: "+widthScale+", "+heightScale);
-        System.out.println("final size: "+this.width*widthScale+", "+this.height*heightScale);
+//        System.out.println("original size: "+image.getWidth()+", "+image.getHeight());
+//        System.out.println("target* size: "+this.width+", "+this.height);
+//        System.out.println("scale: "+widthScale+", "+heightScale);
+//        System.out.println("final size: "+this.width*widthScale+", "+this.height*heightScale);
 
-        transform.translate(posX-((double)image.getWidth()*widthScale/2), posY-((double)image.getHeight()*heightScale/2));
+        transform.translate(posX/*-((double)image.getWidth()*widthScale/2)*/, posY/*-((double)image.getHeight()*heightScale/2)*/);
 
         transform.scale(widthScale, heightScale);
 
-
+        g2d.drawImage(image2, transform, null);
+        if(opacity > 1.0f){
+            opacity = 1.0f;
+        }
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity));
         g2d.drawImage(image, transform, null);
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+
     }
 }
